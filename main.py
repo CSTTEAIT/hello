c99b8d816aec10f6296937e23cbe154d977bcf90
YaYa <hana12312347@csttea.com>
2026-01-22 17:16:58 +0800

    ha

--- main.py
+++ main.py
@@ -2,8 +2,5 @@ print('hello world')
 print('hello world3')
 print('hello world4')
 print('hello world5')
-<<<<<<< HEAD
 print('hello world6')
-=======
 print('hello world7')
->>>>>>> a1404cc6bbd44b284084851581554bf2e7596715
